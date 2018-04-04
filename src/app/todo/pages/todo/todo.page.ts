a7fa382bebf499d1b8a4cf66ccfb3406aea37d86
Tom Opdebeeck <tom.opdebeeck@district01.be>
2018-04-04 11:01:13 +0200

    cleanup with @select

--- src/app/todo/pages/todo/todo.page.ts
+++ src/app/todo/pages/todo/todo.page.ts
@@ -1,30 +1,30 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
-import { NgRedux } from "@angular-redux/store";
+import { select } from "@angular-redux/store";
 import { ActivatedRoute } from "@angular/router";
+import { Observable } from "rxjs/Observable";
 import { Subject } from "rxjs/Subject";
 import "rxjs/add/operator/takeUntil";
 
-import { Todo, TodoState, TodoActioncreator } from "../../store";
+import { Todo, TodoActioncreator } from "../../store";
 
 @Component({
   selector: "todo-page",
   templateUrl: "./todo.page.html",
 })
 export class TodoPageComponent implements OnInit, OnDestroy {
-  public todo: Todo = null;
+  @select("todo") public todo$: Observable<Todo>;
   public loading: boolean = true;
 
+  private todo: Todo;
   private destroy$: Subject<boolean> = new Subject<boolean>();
 
   constructor(
     private route: ActivatedRoute,
-    private ngRedux: NgRedux<TodoState>,
     private todoActions: TodoActioncreator
   ) {}
 
   public ngOnInit(): void {
-    this.ngRedux
-      .select("todo")
+    this.todo$
       .takeUntil(this.destroy$)
       .subscribe((todo: Todo) => this.todo = todo);
 
--- src/app/todo/pages/todos/todos.page.ts
+++ src/app/todo/pages/todos/todos.page.ts
@@ -1,32 +1,27 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
-import { NgRedux } from "@angular-redux/store";
+import { select } from "@angular-redux/store";
+import { Observable } from "rxjs/Observable";
 import { Subject } from "rxjs/Subject";
 import "rxjs/add/operator/takeUntil";
 
-import { Todos, TodoState, TodosActioncreator, Todo, TodoActioncreator } from "../../store";
+import { Todos, TodosActioncreator, Todo, TodoActioncreator } from "../../store";
 
 @Component({
   selector: "todos-page",
   templateUrl: "./todos.page.html",
 })
 export class TodosPageComponent implements OnInit, OnDestroy {
-  public todos: Todos = [];
+  @select("todos") public todos$: Observable<Todos>;
   public loading: boolean = true;
 
   private destroy$: Subject<boolean> = new Subject<boolean>();
 
   constructor(
-    private ngRedux: NgRedux<TodoState>,
     private todosActions: TodosActioncreator,
     private todoActions: TodoActioncreator
   ) {}
 
   public ngOnInit(): void {
-    this.ngRedux
-      .select("todos")
-      .takeUntil(this.destroy$)
-      .subscribe((todos: Todos) => this.todos = todos);
-
     this.todosActions.fetchTodos().then(
       () => this.loading = false,
       () => this.loading = false
